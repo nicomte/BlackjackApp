20e5e491099a98b3fb517553e65d3296802bf635
Marco <marco.petrantoni@stud.hslu.ch>
2024-11-13 16:30:24 +0100

    gugus

---
+++ Blackjack/Blackjack/BlackjackApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlackjackApp.swift
+//  Blackjack
+//
+//  Created by HSLU-N0004891 on 13.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct BlackjackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
